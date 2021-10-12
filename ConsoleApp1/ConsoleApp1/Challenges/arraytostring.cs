536f3c3038dbc1f01a1fa89d0810597595b71fb5
Praful Shrestha <prafulshrestha.softace@outlook.com>
2021-10-12 12:09:07 +0545

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Challenges/arraytostring.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Challenges
+{
+    public class arraytostring
+    {
+        public static void ParseArray(int arr)
+        {
+            Console.WriteLine(arr);
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ConsoleApp1.Challenges;
+
+namespace ConsoleApp1
+{
+    class Program : arraytostring
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ParseArray(5);
+        }
+
+    }
+}
