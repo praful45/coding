9c1a7afd5d1190bb2af55d25fc3818b66937f99e
Praful Shrestha <prafulshrestha.softace@outlook.com>
2021-10-26 14:51:43 +0545

    single number

---
+++ LeetCode/c++/LC-136.cpp
@@ -0,0 +1,13 @@
+//leetcode 136. Single Number
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int res=0;
+        for(int i=0;i<nums.size();i++) {
+            res = res ^ nums[i]; //XOR
+            // res ^= nums[i]; //short form
+        }
+        return c;
+    }
+};
